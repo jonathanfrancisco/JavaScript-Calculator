f5089a0636458b1cdf75af34deb10eea461dec4e
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2018-02-19 03:30:33 +0800

    Refactor

--- app.js
+++ app.js
@@ -11,64 +11,30 @@ function calculate(toEval) {
 
 calculatorDiv.addEventListener('click', function(event) {
 
+	var reg = RegExp('[.x+*/()%-]');
 	var buttonClicked = event.target;
 
 	if(buttonClicked.tagName == "BUTTON") {
 
-
 		var input = buttonClicked.textContent;
 
-		
-		if(input == "=") {
-
-			if(text!="") {
-
-
-				try {
-
-					if(calculate(text) == "Infinity")  {
-						text = "Cannot divide by zero";
-					}
-
-					else {
-						text = calculate(text);
-					}
-					
-				}
-
-				catch(error) {
-					console.log(error);
-				}
-				
-			}
-
-
-		}
-
-		else if(input == "Clear") {
+		if(input == "=" && text !== "") 
+			if(calculate(text) === Infinity)
+				text = "Cannot Divide by Zero.";
+			else
+				text = calculate(text); 
+		else if(input == "Clear") 
 			text = "";
-		} 
-
-		else if(   (input == "+" || input == "-" || input == "*" || input == "/" || input == "%" || input == ".") && text.length == 0  ){
-			// do nothing
+		else if(reg.test(text) && text.length == 0  )
 			alert("WHAT");
-		}
-
 		else  {
-				
-				var lastChar = text.toString().charAt(text.length-1);
-
-				if( (lastChar == "+" || lastChar == "-" || lastChar == "*" || lastChar == "/" || lastChar == "%" || lastChar == ".") && (input == "+" || input == "-" || input == "*" || input == "/" || input == "%" || input == "." || input == "(" || input == ")")) {
-					
-					var newString = text.slice(0,text.length-1)+input;
-					text = newString;
-
-				}
-
-				else {
-					text += input;
-				}
-
+			var lastChar = text.toString().charAt(text.length-1);
+			if (reg.test(lastChar) && reg.test(input)) {
+				var newString = text.slice(0,text.length-1)+input;
+				text = newString;
+			}
+			else 
+				text += input;
 		}
 
 		calculatorScreenText.textContent = text;
